be0f3eeabbacf2d6f4bbb071a3a284c355929355
Sven Bergner <sven.bergner@wolterskluwer.com>
2024-04-02 20:32:05 +0200

    customized look

--- status/git.sh
+++ status/git.sh
@@ -1,10 +1,9 @@
 show_git() {
   local index=$1
+  local text=$(get_tmux_option "@gruvbox_git_text" '#( gitmux -cfg $HOME/.gitmux.conf "#{b:pane_current_path}" )')
+
   local icon=$(get_tmux_option "@gruvbox_git_icon" "")
-
   local color=$(get_tmux_option "@gruvbox_git_color" "$thm_red")
-  local text=$(get_tmux_option "@gruvbox_git_text" '#( gitmux "#{b:pane_current_path}" )')
-
   local module=$(build_status_module "$index" "$icon" "$color" "$text")
 
   echo "$module"
